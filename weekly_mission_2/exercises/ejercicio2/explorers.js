a3cf6c4e9995238105b2b9559e65895a7cbdee10
EduNieto <edjnieto@gmail.com>
2022-04-22 16:03:12 -0500

    update: actualizando ejercicio2

--- weekly_mission_2/exercises/ejercicio2/explorers.js
+++ weekly_mission_2/exercises/ejercicio2/explorers.js
@@ -70,27 +70,35 @@ function getStack() {
 getStack();
 
 // Crea una nueva lista con las listas de stacks de cada explorer, usa MAP
-const explorerStacks = explorers.map(explorer => explorer.stack);
+const explorerStacks = explorers.map((explorer) => explorer.stack);
 console.log("Creando lista de stacks con map: ", explorerStacks);
 
 // Obtén la lista de explorers que tengan en su stack "js", usa FILTER (para validar un elemento en un lista se usa el método includes)
 const stacks = explorers.filter((explorer) => explorer.stack.includes("js"));
-console.log("Objetos que incluyen JS", stacks)
+console.log("Objetos que incluyen JS", stacks);
 
 // Busca el primer explorer que sea de la CDMX, usa FIND
 const findCity = explorers.find((location) => location.city === "CDMX");
 console.log(`${findCity.name} es el primero de ${findCity.city}`);
 
 // Obtén la suma de todos los exercises_completed, usa REDUCE
-const result = explorers.reduce((acc, exercises) => acc + exercises.exercises_completed, 0);
+const result = explorers.reduce(
+  (acc, exercises) => acc + exercises.exercises_completed,
+  0
+);
 console.log("Suma de todos los ejercios resueltos: ", result);
 
 // Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME
 const exerciseFinished = explorers.some(
   (exercices) => exercices.missions.frontend.exercisesFinished === true
 );
-console.log("Algún explorer ha finalizado la misión frontend? ", exerciseFinished);
+console.log(
+  "Algún explorer ha finalizado la misión frontend? ",
+  exerciseFinished
+);
 
 // Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY.
-const onBoarding = explorers.every((explorer) => explorer.missions.onboarding.isFinished === true);
+const onBoarding = explorers.every(
+  (explorer) => explorer.missions.onboarding.isFinished === true
+);
 console.log("¿Todos los explorers terminaron el on boardring? ", onBoarding);
